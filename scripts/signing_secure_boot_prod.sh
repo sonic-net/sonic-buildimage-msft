89239eb29f33d3ea38db494e3221494a408e286f
Sachin Naik <sachnaik@cisco.com>
2023-02-27 10:21:47 -0800

    backport: secureboot support
       Backporting #12692 PR on 202205 branch.
    
       The diffs of #12692 does not cleanly gets apply.
    
       on 202205 branch. Hence the patch #12692 slightly
    
       modified so that it can get applied on 202205 branch
    
       without functional break.
    
    Signed-off-by: Sachin Naik <sachnaik@cisco.com>

---
+++ scripts/signing_secure_boot_prod.sh
@@ -0,0 +1,2 @@
+# In this script Vendor should code  the logic to build a secure boot image by using production vendor flows.
+# More details can be found in the Secure Boot HLD in https://github.com/sonic-net/SONiC repo

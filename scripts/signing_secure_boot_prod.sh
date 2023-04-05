a7652d8eb5639b12c17290188d465c57a5d47c74
Sachin Naik <sacnaik@gmail.com>
2023-04-05 11:20:48 -0700

    Delete signing_secure_boot_prod.sh

--- scripts/signing_secure_boot_prod.sh
+++
@@ -1,2 +0,0 @@
-# In this script Vendor should code  the logic to build a secure boot image by using production vendor flows.
-# More details can be found in the Secure Boot HLD in https://github.com/sonic-net/SONiC repo

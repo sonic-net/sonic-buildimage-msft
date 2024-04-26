15fc9dc0b403df013f5e289ca9afc3606f2a68ef
abdosi <58047199+abdosi@users.noreply.github.com>
2024-04-26 14:44:56 -0700

    Updated LLDP startup configuration for multi-asic (#15440)
    What I did:
    Changes in LLDP Configuration to handle this:
    
    Enable LLDP only on front panel interfaces.
    Why I did:
    This is done to free up CPU cycle as we don't see use for that in multi-asic and also for Chassis we have disable lldp docker on supervisor so there is no lldp peering even if send LLDP BPDU on backplane ports
    
    For multi-asic do not generate lldpd.conf with eth0 related configuration
    Why I did:
    In namespace lldp docker there is no need to generate eth0 configuration. It gives not needed errror message as seen here https://github.com/sonic-net/sonic-buildimage/issues/15382
    
    How I did it:
    Instead of relying on DEVICE_METADTA sub_role we are using NAMESPACE_ID environmental variable (passed to all docker instance) which will be > 0 for multiasic docker instance and not define for global/host docker instance.
    
    ---------
    
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>

--- dockers/docker-lldp/docker-lldp-init.sh
+++ dockers/docker-lldp/docker-lldp-init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #Generate supervisord.conf based on device metadata
 mkdir -p /etc/supervisor/conf.d/
-sonic-cfggen -d -t /usr/share/sonic/templates/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
+sonic-cfggen -d -a "{\"namespace_id\":\"$NAMESPACE_ID\"}" -t /usr/share/sonic/templates/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
 exec /usr/local/bin/supervisord
